2cce2c402f68382396c35eccf29d1e6873200466
Alexander Rankov <alexanderrankov@gmail.com>
2026-01-20 09:04:22 -0500

    I created three folders for the Model, View and Controller algorithm. I also created a script for the MainFrame and added it to the main file.

---
+++ src/View/MainFrame.java
@@ -0,0 +1,10 @@
+package View;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame implements Runnable{
+    @Override
+    public void run() {
+        this.setVisible(true);
+    }
+}
